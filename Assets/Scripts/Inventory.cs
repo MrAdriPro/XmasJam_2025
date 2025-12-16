c182f12267c1aa7a4537e8c6ec89d63b2e970dc8
Kasukiih <kasukihyakkymaru@gmail.com>
2025-12-16 14:39:58 +0100

    primera vez q me entero dictionary

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,5 +3,17 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-   //Proximamente
+    Dictionary<string, int> _inventory = new Dictionary<string, int>();
+
+    private void Start()
+    {
+        _inventory.Add("perla", 2);
+
+        _inventory["perla"] = 4;
+
+        if(_inventory["perla"] == 4) 
+        {
+            print("hola perla");
+        }
+    }
 }
