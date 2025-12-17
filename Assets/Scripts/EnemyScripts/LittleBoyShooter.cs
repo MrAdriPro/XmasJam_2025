b7cfda10645fbf64e078463b2d1f2ccecec8a16c
Lolaxete <xlightez2003@gmail.com>
2025-12-17 19:43:54 +0100

    NIÑOOOOOOOS

--- Assets/Scripts/EnemyScripts/LittleBoyShooter.cs
+++ Assets/Scripts/EnemyScripts/LittleBoyShooter.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 
 public class LittleBoyShooter : MonoBehaviour
@@ -8,9 +7,8 @@ public class LittleBoyShooter : MonoBehaviour
     [SerializeField] private EnemyStats enemyStats;
     private Animator animator;
 
-    public float attackRate = 2f;
-
     private float timer;
+    private float attackRate;
 
     private void Awake()
     {
@@ -24,42 +22,31 @@ public class LittleBoyShooter : MonoBehaviour
         {
             timer += Time.deltaTime;
 
-            if (timer > attackRate) 
+            if (timer >= attackRate) 
             {
-                
-                Shoot();
+                animator.SetTrigger("isAttacking");
+                timer = 0f; 
             }
         }
-        else if(enemyController.actionActive == false)
+        else 
         {
             timer = 0f;
-            print("Shootn't");
-            
         }   
     }
 
-    private void Shoot() 
+    public void ExecuteShoot() 
     {
-        print("Shoot!");
+        if (enemyController.playerCol.Length == 0) return;
 
         Transform targetPoint = enemyController.playerCol[0].transform;
-
-        //Vector3 dir = targetpoint.position - transform.position;
-
-        //Quaternion quaternion = Quaternion.Euler(dir.x, dir.y, dir.z);
-
+        
         var lookPos = targetPoint.position - transform.position;
         lookPos.y = 0;
         var rotation = Quaternion.LookRotation(lookPos);
 
-        StartCoroutine(Delay());
-        Instantiate(projectilePrefab, transform.position + new Vector3(0f,0.2f,0f) , rotation);
-
-        timer = 0f;
-    }
-    IEnumerator Delay()
-    {
-        animator.SetTrigger("isAttacking");
-        yield return new WaitForSeconds(0.5f);
+        Instantiate(projectilePrefab, transform.position + new Vector3(0f, 0.2f, 0f), rotation);
+        
+        
     }
 }
+
